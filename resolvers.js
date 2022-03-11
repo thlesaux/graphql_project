ecd4e154bd888158b4cfe493d80537b0a004da4b
thlesaux <lesaux.thomas@gmail.com>
2022-03-11 19:03:44 +0100

    second checkpoint

--- resolvers.js
+++ resolvers.js
@@ -19,21 +19,60 @@ const Article = {
 
 const Langue = {
     themesParLangue: (root) => {
-        return db.themes.list().filter(function(item) {
-            return root.token === item.code_langue;
-        });
+        return db.themes.list().filter(function (item) {
+            return root.token === item.code_langue
+        })
     },
+    sourcesScrapParLangue: (root) => {
+        return db.sources_scrap.list().filter(function (item) {
+            const source = db.sources.list().find((source) =>
+                source.token === item.token_source
+            )
+            return source.token_langue === root.token
+        })
+    },
+    articlesScrapParLangue: (root) => {
+        let sourcesScrapParLangue = db.sources_scrap.list().filter(function (item) {
+            const source = db.sources.list().find((source) =>
+                source.token === item.token_source
+            )
+            return source.token_langue === root.token
+        })
+
+        return db.articles.list().filter(function (item) {
+            if (sourcesScrapParLangue.length > 0) {
+                return sourcesScrapParLangue.find((source_scrap) =>
+                    source_scrap.token === item.token_source_scrap
+                )?.token === item.token_source_scrap
+            }
+        })
+    }
 }
 
 const Pays = {
     sourcesScrapParPays: (root) => {
-        let sources = db.sources_scrap.list().filter(function(item) {
-            return db.sources.list().find(function (source) {
-                return source.token === item.token_source
-            })
-        });
+        return db.sources_scrap.list().filter(function (item) {
+            const source = db.sources.list().find((source) =>
+                source.token === item.token_source
+            )
+            return source.token_pays === root.token
+        })
+    },
+    articlesScrapParPays: (root) => {
+        let sourcesScrapParPays = db.sources_scrap.list().filter(function (item) {
+            const source = db.sources.list().find((source) =>
+                source.token === item.token_source
+            )
+            return source.token_pays === root.token
+        })
 
-        console.log(sources)
+        return db.articles.list().filter(function (item) {
+            if (sourcesScrapParPays.length > 0) {
+                return sourcesScrapParPays.find((source_scrap) =>
+                    source_scrap.token === item.token_source_scrap
+                )?.token === item.token_source_scrap
+            }
+        })
     }
 }
 
@@ -67,6 +106,19 @@ const Theme = {
     langue: (root) => {
         return db.langues.list().find(function (item) {
             return item.token === root.code_langue
+        })
+    },
+    articlesParTheme: (root) => {
+        const sourcesScrapParTheme = db.sources_scrap.list().filter(function (item) {
+            return item.token_theme === root.token
+        })
+
+        return db.articles.list().filter(function (item) {
+            if (sourcesScrapParTheme.length > 0) {
+                return sourcesScrapParTheme.find((source_scrap) =>
+                    source_scrap.token === item.token_source_scrap
+                )?.token === item.token_source_scrap
+            }
         })
     }
 }
